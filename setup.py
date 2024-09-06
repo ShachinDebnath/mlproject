4acc85cd8942783892c1afc32db6aa54550c9a3d
Shachin Debnath <shachindebnath444@gmail.com>
2024-09-06 21:55:40 +0600

    adding notebook

--- setup.py
+++ setup.py
@@ -1,30 +1,28 @@
-from setuptools import find_packages,setup
+from setuptools import find_packages, setup
 from typing import List
 
-HYPEN_E_DOT='-e .'
+HYPEN_E_DOT = '-e .'
 
-def get_requirements(file_path:str)->list[str]:
+def get_requirements(file_path: str) -> List[str]:
     '''
-    this function will return the list of requirements
+    This function will return the list of requirements
     '''
-    requirements =[]
-    with open (file_path) as file_obj:
-        requirements=file_obj.readlines()
-        requirements= [req.replace('/n','') for req in requirements]
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace('\n', '') for req in requirements]
         
         # Removing '-e .' from requirements.txt list
         if HYPEN_E_DOT in requirements: 
             requirements.remove(HYPEN_E_DOT)
             
     return requirements
-        
+
 setup(
-name= 'mlproject',
-version='0.0.1',
-author='shachin',
-author_email='shachindebnath444@gmail.com',
-pacages = find_packages(),
-install_requires=get_requirements('requirements.txt')
-
-
+    name='mlproject',
+    version='0.0.1',
+    author='shachin',
+    author_email='shachindebnath444@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt'),
 )
